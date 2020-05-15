afa489788b91420614992a760936493b3ab53e45
kazenomonmon99 <63276131+kazenomonmon99@users.noreply.github.com>
2020-05-15 23:12:08 +0900

    tweaked readme file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
   root 'home#index'
+  get 'home/about'
+  get 'home/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
